b992eed4c64301f754e05d955687888e88d8e388
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-07-14 10:58:51 -0400

    m

--- pets.js
+++ pets.js
@@ -1,32 +1,69 @@
-import fs from 'fs'
+// import {writeFile} from 'fs';
+import {readFile, writeFile} from 'fs/promises';
+
 const subCommand = process.argv[2]
+const index = process.argv[3]
 
-// if (subCommand == 'read') {
+switch (subCommand) {
+    case 'read':
+        {
 
-// }
-// else if (subCommand == 'create') {
-// }
-// else if (subCommand == 'update') {
-// }
-// else if (subCommand == 'destroy') {
-// }
+            readFile('pets.json', 'utf-8').then(str => {
+                let data = JSON.parse(str);
+                if (index) {
+                    console.log(data[index]);
+                } else {
+                    console.log(data);
+                }
 
-switch(subCommand) {
-    case 'read': {
-        fs.readFile('./pets.json', 'utf-8', (err, data) => {
-            const data = JSON.parse(str);
-            console.log(data[0], 'data')
-    })
-}
+            });
+
+            break;
+        }
     case 'create':
+        // below writes to pets.JSON but replaces previous entries with the created entry.
+        {
+            let [index, age, kind, name] = process.argv;
+            readFile('pets.json', 'utf-8').then(str => {
+                let data = JSON.parse(str);
+                if (!data[index]) {
+                    const pet = {}
+                    data.concat(pet)
+                    pet.age = Number(`${process.argv[3]}`);
+                    pet.kind = process.argv[4];
+                    pet.name = process.argv[5];
+                    let petStr = JSON.stringify(pet);
+                    writeFile('pets.json', petStr, function(error){
+                        if(error) {
+                            console.log(error)
+                        } else {
+                            console.log(pet)
+                        }
+                    })
+                
+                } else {
+                    console.error('Usage: node pets.js create INDEX AGE KIND NAME')
+                    process.exit(1);
+                }
+            });
+            
+        }
+        break;
+
     case 'update':
+        {
+
+            break;
+        }
     case 'destroy':
-    default: {
-        console.log('Usage: node pets.js [read|create|update|destroy]')
-    }
+        {
+
+            break;
+        }
+    default:
+        {
+            console.log('Usage: node pets.js [read | create | update | destroy]');
+            process.exit(1);
+
+        }
 }
-
-
-// console.log(subCommand);
-
-// console.error('Usage: node pets.js [read | create | update | destroy]');
