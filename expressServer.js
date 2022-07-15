98f05fd119fc35678542ceaa33f74727ebf88a02
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-07-15 18:32:25 -0400

    m

--- expressServer.js
+++ expressServer.js
@@ -1,48 +1,45 @@
 import express from "express";
-// import { readFile } from "fs/promises";
-import { readPetsFile } from "./shared.js";
+import {writeFile} from "fs/promises";
+import {readPetsFile} from "./shared.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.get("/pets", async (req, res, next) => {
+app.get("/pets", async (res, next) => {
     const pets = await readPetsFile().catch(next);
-       res.send(pets); 
-    });
-    
+    res.send(pets);
+});
+
 app.get("/pets/:index", (req, res, next) => {
     const index = req.params.index;
-    readPetsFile()
-    .then((pets) => {
+    readPetsFile().then((pets) => {
         if (pets[index]) {
             res.send(pets[index]);
-            } else {
-                res.send (404);
-                res.send (`Invalid index given: ${index}`);
+        } else {
+            res.send(404);
+            res.send(`Invalid index: ${index}`);
         }
-})
-.catch(next);
+    }).catch(next);
 });
 
 app.post("/pets", (req, res, next) => {
-    const body = req.body;
-    readPetsFile()
-    .then((str) => {
-        const pets = JSON.parse(str);
-        pets.push(body);
-        res.send(pets);
-})
-.catch(next);
+    const newPet = req.body;
+    readPetsFile().then((pets) => {
+        const newPets = pets.concat(newPet);
+        return writeFile("pets.json", JSON.stringify(newPets));
+    }).then(() => {
+        res.send(newPet);
+    }).catch(next);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, res) => {
     if (err) {
         res.status(500);
     }
 });
 
 app.listen(port, () => {
-    console.log("Server running at", port)
+    console.log("Server serving at", port)
 })
